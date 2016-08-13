f92e45cd299baa7eb16661f4ba3ea54db0667832
felix <felix@volny.co>
2016-08-14 00:22:48 +0700

    added conditionals to support incomplete api responses

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function getJSON(url, callback) {
 
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
-      // Success!
       callback(JSON.parse(request.responseText));
     } else {
       console.error("reached " + url + ", but it returned an error")
@@ -24,39 +23,51 @@ function getLocation(callback) {
   getJSON(url, function(data) {
     if (data.city && data.country_name) {
       callback(data.city + ', ' + data.country_name);
-    } else {
-      console.debug('no location in IP, reverse geocoding...');
+    } else if (data.latitude && data.longitude) {
+      console.debug('no placenames found, reverse geocoding...');
       var baseUrl = "https://nominatim.openstreetmap.org/reverse?format=json"
       var revUrl = baseUrl + '&lat=' + data.latitude + '&lon=' + data.longitude;
 
       getJSON(revUrl, function(data) {
         callback(data.address.state + ', ' + data.address.country);
-      })
+      });
+    } else {
+      console.debug("couldn't get placename or coordinates found, say sorry");
+      callback("");
     }
   });
 }
 
 function getWeather(callback) {
   getLocation(function(location) {
-    var query = 'select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")';
-    var url = "https://query.yahooapis.com/v1/public/yql" + "?q=" + encodeURIComponent(query) + "&format=json";
+    if (location) {
+      var query = 'select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")';
+      var url = "https://query.yahooapis.com/v1/public/yql" + "?q=" + encodeURIComponent(query) + "&format=json";
 
-    getJSON(url, function(data) {
-      var condition = data.query.results.channel.item.condition;
-      callback(condition, location);
-    })
+      getJSON(url, function(data) {
+        var condition = data.query.results.channel.item.condition;
+        callback(condition, location);
+      });
+    } else {
+      callback({}, "");
+    }
   })
 }
 
 getWeather(function(condition, location) {
-    console.log(condition.temp + 'F' + ', ' + condition.text + ' in ' + location + ' on ' + condition.date);
-    var _location = document.querySelector('#location');
-    var date = document.querySelector('#date');
-    var temperature = document.querySelector('#temperature');
-    var text = document.querySelector('#text');
+    if (condition && location) {
+      console.log(condition.temp + 'F' + ', ' + condition.text + ' in ' + location + ' on ' + condition.date);
+      var _location = document.querySelector('#location');
+      var date = document.querySelector('#date');
+      var temperature = document.querySelector('#temperature');
+      var text = document.querySelector('#text');
 
-    _location.innerHTML = location;
-    date.innerHTML = condition.date;
-    temperature.innerHTML = condition.temp + 'F';
-    text.innerHTML = condition.text;
+      _location.innerHTML = location;
+      date.innerHTML = condition.date;
+      temperature.innerHTML = condition.temp + 'F';
+      text.innerHTML = condition.text;
+    } else {
+      var error = document.querySelector("#error");
+      error.innerHTML = "Sorry, we couldn't determine your location";
+    }
 })
