0f2bd50af61b5c828cceee1b470413cef7a09d5b
felix <felix@volny.co>
2016-08-13 21:15:39 +0700

    hacky display data

--- script.js
+++ script.js
@@ -8,24 +8,24 @@ function getJSON(url, callback) {
       // Success!
       callback(JSON.parse(request.responseText));
     } else {
-      console.error("reached" + url + ", but it returned an error")
+      console.error("reached " + url + ", but it returned an error")
     }
   };
 
   request.onerror = function() {
-    console.error("connection error in request to" + url)
+    console.error("connection error in request to " + url)
   };
 
   request.send();
 }
 
-function getCity(callback) {
+function getLocation(callback) {
   var url = "https://freegeoip.net/json/";
   getJSON(url, function(data) {
-    if (data.city) {
+    if (data.city && data.country_name) {
       callback(data.city + ', ' + data.country_name);
     } else {
-      console.debug('no city in IP, reverse geocoding...');
+      console.debug('no location in IP, reverse geocoding...');
       var baseUrl = "https://nominatim.openstreetmap.org/reverse?format=json"
       var revUrl = baseUrl + '&lat=' + data.latitude + '&lon=' + data.longitude;
 
@@ -36,12 +36,27 @@ function getCity(callback) {
   });
 }
 
-getCity(function(city) {
-  var query = 'select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + city + '")';
-  var url = "https://query.yahooapis.com/v1/public/yql" + "?q=" + encodeURIComponent(query) + "&format=json";
+function getWeather(callback) {
+  getLocation(function(location) {
+    var query = 'select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")';
+    var url = "https://query.yahooapis.com/v1/public/yql" + "?q=" + encodeURIComponent(query) + "&format=json";
 
-  getJSON(url, function(data) {
-    var condition = data.query.results.channel.item.condition;
-    console.log(condition.temp + 'F' + ', ' + condition.text + ' in ' + city + ' on ' + condition.date);
+    getJSON(url, function(data) {
+      var condition = data.query.results.channel.item.condition;
+      callback(condition, location);
+    })
   })
+}
+
+getWeather(function(condition, location) {
+    console.log(condition.temp + 'F' + ', ' + condition.text + ' in ' + location + ' on ' + condition.date);
+    var _location = document.querySelector('#location');
+    var date = document.querySelector('#date');
+    var temperature = document.querySelector('#temperature');
+    var text = document.querySelector('#text');
+
+    _location.innerHTML = location;
+    date.innerHTML = condition.date;
+    temperature.innerHTML = condition.temp + 'F';
+    text.innerHTML = condition.text;
 })
