2dce18a0d96e0c757b470ddc4685f43aefd2ba70
felix <felix@volny.co>
2016-08-15 15:28:38 +0700

    design done, not yet responsive

--- script.js
+++ script.js
@@ -71,3 +71,62 @@ function getWeather(callback) {
 //       error.innerHTML = "Sorry, we couldn't determine your location";
 //     }
 // })
+
+function getIconName(code) {
+  switch(code) {
+    case '0': return 'wi wi-tornado';
+    case '1': return 'wi wi-storm-showers';
+    case '2': return 'wi wi-tornado';
+    case '3': return 'wi wi-thunderstorm';
+    case '4': return 'wi wi-thunderstorm';
+    case '5': return 'wi wi-snow';
+    case '6': return 'wi wi-rain-mix';
+    case '7': return 'wi wi-rain-mix';
+    case '8': return 'wi wi-sprinkle';
+    case '9': return 'wi wi-sprinkle';
+    case '10': return 'wi wi-hail';
+    case '11': return 'wi wi-showers';
+    case '12': return 'wi wi-showers';
+    case '13': return 'wi wi-snow';
+    case '14': return 'wi wi-storm-showers';
+    case '15': return 'wi wi-snow';
+    case '16': return 'wi wi-snow';
+    case '17': return 'wi wi-hail';
+    case '18': return 'wi wi-hail';
+    case '19': return 'wi wi-cloudy-gusts';
+    case '20': return 'wi wi-fog';
+    case '21': return 'wi wi-fog';
+    case '22': return 'wi wi-fog';
+    case '23': return 'wi wi-cloudy-gusts';
+    case '24': return 'wi wi-cloudy-windy';
+    case '25': return 'wi wi-thermometer';
+    case '26': return 'wi wi-cloudy';
+    case '27': return 'wi wi-night-cloudy';
+    case '28': return 'wi wi-day-cloudy';
+    case '29': return 'wi wi-night-cloudy';
+    case '30': return 'wi wi-day-cloudy';
+    case '31': return 'wi wi-night-clear';
+    case '32': return 'wi wi-day-sunny';
+    case '33': return 'wi wi-night-clear';
+    case '34': return 'wi wi-day-sunny-overcast';
+    case '35': return 'wi wi-hail';
+    case '36': return 'wi wi-day-sunny';
+    case '37': return 'wi wi-thunderstorm';
+    case '38': return 'wi wi-thunderstorm';
+    case '39': return 'wi wi-thunderstorm';
+    case '40': return 'wi wi-storm-showers';
+    case '41': return 'wi wi-snow';
+    case '42': return 'wi wi-snow';
+    case '43': return 'wi wi-snow';
+    case '44': return 'wi wi-cloudy';
+    case '45': return 'wi wi-lightning';
+    case '46': return 'wi wi-snow';
+    case '47': return 'wi wi-thunderstorm';
+    case '3200': return 'wi wi-cloud';
+    default: return 'wi wi-cloud';
+  }
+}
+
+var icon = document.createElement("i");
+icon.className = getIconName('32');
+document.querySelector(".icon").appendChild(icon);
